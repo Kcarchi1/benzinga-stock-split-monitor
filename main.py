66adedf9615f8ecc1cb99f0b751b12e7e522104d
kcarchi1 <kcarchi2@gmail.com>
2023-12-16 18:23:48 -0600

    refactor: removed debugging printing statements for clarity

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import json
-import time
-import redis
-from pprint import pprint
-
 from monitors import BenzingaMonitor, HedgeFollowMonitor, StockAnalysisMonitor
 from monitorservice import MonitorService
 
@@ -11,8 +6,6 @@ monitors = [
     HedgeFollowMonitor, 
     StockAnalysisMonitor,
 ]
-
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
 def main():
@@ -24,24 +17,7 @@ def main():
         service.monitor_strategy = monitor
         service.run()
 
-    pprint(service.storage)
-    print(len(service.storage))
-    service.filter_duplicates()
-    print("to")
-    print(len(service.storage))
-    # with open("output.json", "w") as f:
-    #     json.dump(service.storage, f)
-    # service.sleep()
-    start_time = time.time()
-    sample = json.dumps(service.storage)
-    print (f"JSON Dumps:{time.time() - start_time}")
-    r.set('foo', sample)
-    s = r.get('foo')
-    start_time = time.time()
-    _ = json.loads(s)
-    print(f"JSON Loads:{time.time() - start_time}")
-    print(_)
-    print(type(_))
 
 if __name__ == "__main__":
     main()
+    
